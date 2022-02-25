5336a7a1f91d27eb9857e7febdd0bbea5e20cf30
Triggered41 <vehdathamid@gmail.com>
2022-02-25 19:51:20 +0530

    files added

---
+++ logic.js
@@ -0,0 +1,10 @@
+
+var hm = document.getElementsByClassName("hamburger");
+var w = document.getElementById("wr")
+
+
+function clicked(a)
+{
+    w.classlist.remove("wrapper")
+    console.log(w.disabled);
+}
